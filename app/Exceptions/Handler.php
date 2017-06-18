efdfe2ce78e772d63b0cf20c666feff40eaf678a
corpjorge <corpjorge@hotmail.com>
2017-06-17 18:34:17 -0700

    1

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,7 +46,7 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         //return parent::render($request, $exception);
-        return redirect()->guest('login');
+        return redirect()->guest('login');dd
     }
 
     /**
