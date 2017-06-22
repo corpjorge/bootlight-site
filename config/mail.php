72af253bdfa2130fb205226854b3749fb4ab3a7c
corpjorge <corpjorge@hotmail.com>
2017-06-22 18:56:53 -0500

    mail

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'webmaster@fonsodi.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
