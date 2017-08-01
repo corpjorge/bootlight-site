245e3030fe257558337c092066d728a5dafcf12e
corpjorge <corpjorge@hotmail.com>
2017-08-01 11:35:01 -0500

    tipo text

--- database/migrations/2017_04_15_160505_create_pqrs_table.php
+++ database/migrations/2017_04_15_160505_create_pqrs_table.php
@@ -18,7 +18,7 @@ class CreatePqrsTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->string('tipo');
-            $table->string('descripcion');
+            $table->text('descripcion');
             $table->string('archivo');
             $table->integer('estado_id')->unsigned();
             $table->foreign('estado_id')->references('id')->on('estados_aprobaciones');
