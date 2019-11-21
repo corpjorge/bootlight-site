419fd68ceaefcf4d9ca12e83a4833b7439edaaaa
Jorge Eduardo <corpjorge@hotmail.com>
2019-11-21 13:12:15 -0500

    Reinicio

--- resources/views/vendor/adminlte/admin-auth/emails/password.blade.php
+++
@@ -1,3 +0,0 @@
-{{-- resources/views/emails/password.blade.php --}}
-
-{{ trans('adminlte_lang::message.passwordclickreset') }} <a href="{{ url('admin_password/reset/'.$token) }}">{{ url('admin_password/reset/'.$token) }}</a>
