56f6ddce04a342602636b9683fac70c38e1f05a6
Jorge Eduardo <corpjorge@hotmail.com>
2022-01-27 16:16:20 -0500

    reset

--- resources/views/vendor/adminlte/admin/areas_admin/atras.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="row">
-  <a href="{{url('admin_config/areas_admin/')}}" >
-     <div class="col-md-1">
-         <span class="info-box-icon bg-yellow"><i class="fa fa-chevron-left"></i></span>
-     </div>
-  </a>
-</div><br>
